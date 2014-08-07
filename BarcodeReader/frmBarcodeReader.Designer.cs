1339b8edc38e506372389ac297f37c2ad84ab387
Graham Pye <frahamp@gmail.com>
2014-08-07 20:30:04 +0100

    Added a button and label
    The button will open the barcode image file and the label will display
    the image.

--- BarcodeReader/frmBarcodeReader.Designer.cs
+++ BarcodeReader/frmBarcodeReader.Designer.cs
@@ -28,20 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.lblBarcodeText = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(12, 12);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "Open File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            // 
+            // lblBarcodeText
+            // 
+            this.lblBarcodeText.AutoSize = true;
+            this.lblBarcodeText.Location = new System.Drawing.Point(12, 76);
+            this.lblBarcodeText.Name = "lblBarcodeText";
+            this.lblBarcodeText.Size = new System.Drawing.Size(77, 13);
+            this.lblBarcodeText.TabIndex = 1;
+            this.lblBarcodeText.Text = "Barcode Text: ";
             // 
             // frmBarcodeReader
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lblBarcodeText);
+            this.Controls.Add(this.btnOpenFile);
             this.Name = "frmBarcodeReader";
             this.Text = "Barcode Reader";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Label lblBarcodeText;
     }
 }
 
