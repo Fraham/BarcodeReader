0c22a552a4216c2d52f63303a96324f3fd366388
Graham Pye <frahamp@gmail.com>
2014-08-07 20:28:01 +0100

    Added barcode decode code

--- BarcodeReader/frmBarcodeReader.Designer.cs
+++ BarcodeReader/frmBarcodeReader.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmBarcodeReader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "frmBarcodeReader";
+            this.Text = "Barcode Reader";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- BarcodeReader/frmBarcodeReader.cs
+++ BarcodeReader/frmBarcodeReader.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DataMatrix.net;
 
 namespace BarcodeReader
 {
@@ -15,6 +16,22 @@ namespace BarcodeReader
         public frmBarcodeReader()
         {
             InitializeComponent();
+        }
+
+        private string DecodeText(string sFileName)
+        {
+            DmtxImageDecoder decoder = new DmtxImageDecoder();
+            System.Drawing.Bitmap oBitmap = new System.Drawing.Bitmap(sFileName);
+            List<string> oList = decoder.DecodeImage(oBitmap);
+
+            StringBuilder sb = new StringBuilder();
+            sb.Length = 0;
+            foreach (string s in oList)
+            {
+                sb.Append(s);
+            }
+
+            return sb.ToString();
         }
     }
 }
